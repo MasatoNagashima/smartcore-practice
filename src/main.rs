edb816c2baf5f1705c196dc939c624596a1f6a71
Masato Nagashima <yuuoumaishin@akane.waseda.jp>
2022-07-18 03:10:03 +0900

    add smartcore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use smartcore::dataset::iris::load_dataset;
+use smartcore::linalg::naive::dense_matrix::DenseMatrix;
+use smartcore::neighbors::knn_classifier::KNNClassifier;
+use smartcore::metrics::accuracy;
+
 fn main() {
     println!("Hello, world!");
 }
