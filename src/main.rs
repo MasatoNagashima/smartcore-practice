0188f408138946a71c87217f306d2588bccf1b4f
Masato Nagashima <yuuoumaishin@akane.waseda.jp>
2022-07-18 14:58:29 +0900

    update main.rs and add cargo.lock

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,21 @@ use smartcore::neighbors::knn_classifier::KNNClassifier;
 use smartcore::metrics::accuracy;
 
 fn main() {
-    println!("Hello, world!");
+    let irirs_data = load_dataset();
+
+    let x = DenseMatrix::from_array(
+        irirs_data.num_samples,
+        irirs_data.num_features,
+        &irirs_data.data,
+    );
+
+    let y = irirs_data.target;
+
+    let knn = KNNClassifier::fit(
+        &x,
+        &y,
+        Default::default(),
+    ).unwrap();
+    let y_hat = knn.predict(&x).unwrap();
+    println!("accuracy: {}", accuracy(&y, &y_hat));
 }
