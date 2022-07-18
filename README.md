9d8e2d817ba723ef71e4d1de3ae8d7b7325f981c
Masato Nagashima <yuuoumaishin@akane.waseda.jp>
2022-07-18 15:23:46 +0900

    add train_test_split

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# smartcore-practice
+## smartcore-practice
+My practice code using smartcore
+
+## License
+Apache License 2.0
+
+## Author
+Masato Nagashima
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use smartcore::dataset::iris::load_dataset;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::neighbors::knn_classifier::KNNClassifier;
+use smartcore::model_selection::train_test_split;
 use smartcore::metrics::accuracy;
 
 fn main() {
     let irirs_data = load_dataset();
-
     let x = DenseMatrix::from_array(
         irirs_data.num_samples,
         irirs_data.num_features,
         &irirs_data.data,
     );
-
     let y = irirs_data.target;
+    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
 
     let knn = KNNClassifier::fit(
-        &x,
-        &y,
+        &x_train,
+        &y_train,
         Default::default(),
     ).unwrap();
-    let y_hat = knn.predict(&x).unwrap();
-    println!("accuracy: {}", accuracy(&y, &y_hat));
+    let y_pred = knn.predict(&x_test).unwrap();
+    println!("accuracy: {}", accuracy(&y_test, &y_pred));
 }
